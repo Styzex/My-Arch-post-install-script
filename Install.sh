60ebc50407193fbda74f498e54690b86e89d6797
Styzex <vikinator46@gmail.com>
2024-01-19 16:40:11 +0100

    Added my hyprland dotfiles

--- Install.sh
+++ Install.sh
@@ -2,3 +2,5 @@
 # Post install script for my arch setup
 
 sudo pacman -S hyprland && sudo pacman -S yay && sudo pacman -S waybar &&  sudo pacman -S clipman && sudo pacman -S hyprpaper && sudo pacman -S wlogout && sudo pacman -S rofi && sudo pacman -S alacritty && sudo pacman -S thunar && sudo pacman -S firefox && sudo pacman -S grim && sudo pacman -S slurp && sudo pacman -S viewnior && sudo pacman -S nitch && sudo pacman -S btop && sudo pacman -S qt5-wayland && sudo pacman -S qt6-wayland && sudo pacman -S xdg-desktop-portal-hyprland && sudo pacman -S pipewire
+
+git clone https://github.com/Styzex/Hyprland-dotfiles.git
